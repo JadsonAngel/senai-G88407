213824b430099c8218bef634d36927212a12d31e
JadsonAngel <jjadsonangel@gmail.com>
2023-11-23 19:42:16 -0300

    commit-TESTE

---
+++ EXERCICIO.01.py
@@ -0,0 +1,34 @@
+# Variáveis
+nome = "Eric"
+idade = 19
+altura = 1.90
+estudante = True
+
+# Tipos de variáveis
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(estudante))
+# Operadores matemáticos
+soma = 10 + 5
+diferenca = 8 - 7
+produto = 8 * 6
+divisao = 10 / 5
+print("Operadores matemáticos")
+print("Soma:", soma, "Diferença:", diferenca, "Produto:", produto, "Divisão:", divisao)
+
+# Operadores de comparação
+igual = 5 == 7
+diferente = 5 != 8
+maior = 7 > 5
+menor = 8 < 6
+print("Operadores de comparação")
+print("Igual:", igual, "Diferente:", diferente, "Maior:", maior, "Menor:", menor)
+
+# Operadores lógicos
+e = True and False
+ou = True or False
+nao = not True
+
+print("Operadores lógicos")
+print("E:", e, "Ou:", ou, "Não:", nao)
