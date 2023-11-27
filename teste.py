b14988eff37193e7f7328006374c9c7a3a223650
JadsonAngel <jjadsonangel@gmail.com>
2023-11-27 19:27:28 -0300

    commit EXERCICIO.03

---
+++ teste.py
@@ -0,0 +1,14 @@
+input 
+idade = 20
+verdadeiro = True
+falso = False
+print (idade)
+if idade >= 18:
+    print("Usuario é maior de idade", idade)
+else:     
+    print("USUARIO MENOR DE IDADE",idade)
+status = True
+if status:
+    print("Verdadeiro")
+else:
+        print("Falso")
